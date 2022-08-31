76a0041dbfc6077ca8ba188513d5cd3765323c84
rice <pentappingminh@gmail.com>
2022-08-31 19:44:36 +0700

    add dark mode for user page

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -23,15 +23,7 @@ const features = [
 
 export default function Dashboard() {
   const { mode } = useMode();
-  const fetchData = async () => {
-    try {
-      const respsonse = await UserService.getUsers();
-      console.log(respsonse);
-    } catch (err) {}
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
+
   return (
     <div className=" py-24 px-4 grid items-center grid-cols-1 gap-y-16 gap-x-8 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8 lg:grid-cols-2">
       <div>
--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { FILE_PATH } from "../../constant/staticPath";
 import useMode from "../../hooks/useMode";
+import UserService from "../../service/user";
 
 export default function User() {
   const { mode } = useMode();
+  const [users, setUsers] = useState([]);
+  const [pagination, setPagination] = useState({
+    limit: 10,
+    page: 0,
+    totalDocs: 0,
+    totalPages: 0,
+  });
+  const actionListRef = useRef(null);
+  const handleShowAction = () => {
+    if (actionListRef.current.classList.contains("hidden")) {
+      actionListRef.current.classList.remove("hidden");
+      actionListRef.current.classList.add("block");
+    } else {
+      actionListRef.current.classList.remove("block");
+      actionListRef.current.classList.add("hidden");
+    }
+  };
+  const fetchUsers = async () => {
+    try {
+      const response = await UserService.getUsers();
+      if (response?.data?.data) {
+        setUsers(response?.data?.data);
+        setPagination(response?.data?.paginate);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div className="my-10">
       <h2
@@ -12,85 +46,241 @@ export default function User() {
       >
         Users
       </h2>
-      <div className=" overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <div
+        className={`overflow-x-auto relative  sm:rounded-lg ${
+          mode ? "" : "shadow-md"
+        }  `}
+      >
+        <div
+          className={`flex justify-between items-center pb-4 ${
+            mode ? "bg-white" : "dark:bg-gray-900"
+          } `}
+        >
+          <div>
+            <button
+              onClick={() => {
+                handleShowAction();
+              }}
+              className={`inline-flex items-center   font-medium rounded-lg text-sm px-3 py-1.5 ${
+                mode
+                  ? "text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100"
+                  : "dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600"
+              } `}
+              type="button"
+            >
+              <span className="sr-only">Action button</span>
+              Action
+              <svg
+                className="ml-2 w-3 h-3"
+                aria-hidden="true"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </button>
+
+            <div
+              ref={actionListRef}
+              className={`z-10 w-44 hidden ${
+                mode
+                  ? "bg-white rounded divide-y divide-gray-100 shadow"
+                  : "dark:bg-gray-700 dark:divide-gray-600"
+              } `}
+              style={{
+                position: "absolute",
+                inset: "0px auto auto 0px",
+                margin: 0,
+                transform: "translate3d(0px, 46.4px, 0px)",
+              }}
+              data-popper-reference-hidden=""
+              data-popper-escaped=""
+              data-popper-placement="bottom"
+            >
+              <ul
+                className={`py-1 text-sm ${
+                  mode ? "text-gray-700" : "dark:text-gray-200"
+                }  `}
+                aria-labelledby="dropdownActionButton"
+              >
+                <li>
+                  <a
+                    href="#"
+                    className={`block py-2 px-4 ${
+                      mode
+                        ? "hover:bg-gray-100"
+                        : "dark:hover:bg-gray-600 dark:hover:text-white"
+                    }  `}
+                  >
+                    Reward
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className={`block py-2 px-4 ${
+                      mode
+                        ? "hover:bg-gray-100"
+                        : "dark:hover:bg-gray-600 dark:hover:text-white"
+                    }  `}
+                  >
+                    Promote
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className={`block py-2 px-4 ${
+                      mode
+                        ? "hover:bg-gray-100"
+                        : "dark:hover:bg-gray-600 dark:hover:text-white"
+                    }  `}
+                  >
+                    Activate account
+                  </a>
+                </li>
+              </ul>
+              <div className="py-1 ">
+                <a
+                  href="#"
+                  className={` block py-2 px-4 text-sm ${
+                    mode
+                      ? "text-gray-700 hover:bg-gray-100 "
+                      : "dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                  }  `}
+                >
+                  Delete User
+                </a>
+              </div>
+            </div>
+          </div>
+          <label htmlFor="table-search" className="sr-only">
+            Search
+          </label>
+          <div className="relative">
+            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+              <svg
+                className={`w-5 h-5 ${
+                  mode ? "text-gray-500 " : "dark:text-gray-400"
+                } `}
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <input
+              type="text"
+              id="table-search-users"
+              className={`block p-2 pl-10 w-80 text-sm rounded-lg border ${
+                mode
+                  ? "text-gray-900 bg-gray-50  border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
+                  : "dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              }`}
+              placeholder="Search for users"
+            />
+          </div>
+        </div>
+        <table className="w-full text-sm text-left align-middle text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th scope="col" className="px-6 py-3">
-                Product name
+              <th scope="col" className="p-4">
+                <div className="flex items-center">
+                  <input
+                    id="checkbox-all-search"
+                    type="checkbox"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label htmlFor="checkbox-all-search" className="sr-only">
+                    checkbox
+                  </label>
+                </div>
               </th>
-              <th scope="col" className="px-6 py-3">
-                Color
+              <th scope="col" className="py-3 px-6">
+                Name
               </th>
-              <th scope="col" className="px-6 py-3">
-                Category
+              <th scope="col" className="py-3 px-6">
+                Position
               </th>
-              <th scope="col" className="px-6 py-3">
-                Price
+              <th scope="col" className="py-3 px-6">
+                Status
               </th>
-              <th scope="col" className="px-6 py-3">
-                <span className="sr-only">Edit</span>
+              <th scope="col" className="py-3 px-6">
+                Action
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
-              >
-                Apple MacBook Pro 17"
-              </th>
-              <td className="px-6 py-4">Sliver</td>
-              <td className="px-6 py-4">Laptop</td>
-              <td className="px-6 py-4">$2999</td>
-              <td className="px-6 py-4 text-right">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+            {users &&
+              users?.length &&
+              users.map((user, index) => (
+                <tr
+                  key={user?._id}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
                 >
-                  Edit
-                </a>
-              </td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
-              >
-                Microsoft Surface Pro
-              </th>
-              <td className="px-6 py-4">White</td>
-              <td className="px-6 py-4">Laptop PC</td>
-              <td className="px-6 py-4">$1999</td>
-              <td className="px-6 py-4 text-right">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
-              </td>
-            </tr>
-            <tr className="bg-white dark:bg-gray-800">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
-              >
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">Accessories</td>
-              <td className="px-6 py-4">$99</td>
-              <td className="px-6 py-4 text-right">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
-              </td>
-            </tr>
+                  <td className="p-4 w-4 align-middle">
+                    <div className="flex items-center">
+                      <input
+                        id="checkbox-table-search-1"
+                        type="checkbox"
+                        className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                      <label
+                        htmlFor="checkbox-table-search-1"
+                        className="sr-only"
+                      >
+                        checkbox
+                      </label>
+                    </div>
+                  </td>
+                  <th
+                    scope="row"
+                    className="flex items-center py-4 px-6 text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    <img
+                      className="w-10 h-10 rounded-full"
+                      src={`${FILE_PATH + user?.avatar}`}
+                      alt="Jese image"
+                    />
+                    <div className="pl-3">
+                      <div className="text-base font-semibold">
+                        {user?.firstname + " " + user?.lastname}
+                      </div>
+                      <div className="font-normal text-gray-500">
+                        {user?.email}
+                      </div>
+                    </div>
+                  </th>
+                  <td className="py-4 px-6 align-middle">React Developer</td>
+                  <td className="py-4 px-6 align-middle">
+                    <div className="flex items-center">
+                      <div className="h-2.5 w-2.5 rounded-full bg-green-400 mr-2" />{" "}
+                      Online
+                    </div>
+                  </td>
+                  <td className="py-4 px-6 align-middle">
+                    <a
+                      href="#"
+                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      Edit user
+                    </a>
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
