676c5e4cf25778b1a95f6b1e7bb0a3547fdba90e
rice <pentappingminh@gmail.com>
2022-09-09 00:49:32 +0700

    log test prod

--- src/helper/router.js
+++ src/helper/router.js
@@ -3,5 +3,6 @@ import { ROUTER, ROUTERS } from "../constant/router";
 export const isValidRouter = () => {
   const path = window.location.href.split("/");
   const currentRouter = "/" + path[3];
+  console.log(currentRouter);
   return ROUTERS.includes(currentRouter);
 };
