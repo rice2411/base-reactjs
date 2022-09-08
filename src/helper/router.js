b93faefd07ab5033d9404e3cb746ebd068f5efd3
rice <pentappingminh@gmail.com>
2022-09-09 00:56:56 +0700

    hotfix/bug router

--- src/helper/router.js
+++ src/helper/router.js
@@ -2,7 +2,6 @@ import { ROUTER, ROUTERS } from "../constant/router";
 
 export const isValidRouter = () => {
   const path = window.location.href.split("/");
-  const currentRouter = "/" + path[3];
-  console.log(ROUTERS.includes(currentRouter));
+  const currentRouter = ROUTER.SLASH + path[3];
   return ROUTERS.includes(currentRouter);
 };
