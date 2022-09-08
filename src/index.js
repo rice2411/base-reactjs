eb2c09f932b1111f11e234bef13bdd0b0bb703ab
rice <pentappingminh@gmail.com>
2022-09-08 15:32:24 +0700

    hotfix/spa react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import { AuthProvider } from "./context/auth";
 import { ModeProvider } from "./context/mode";
@@ -17,9 +17,9 @@ root.render(
       <AuthProvider>
         <ModeProvider>
           <ModalProvider>
-            <Routes>
+            <HashRouter>
               <Route path="/*" element={<App />} />
-            </Routes>
+            </HashRouter>
           </ModalProvider>
         </ModeProvider>
       </AuthProvider>
