e595bfd127511362d8b6b1694812c2168560ff50
rice <pentappingminh@gmail.com>
2022-09-08 12:10:04 +0700

    hotfix/ spa react

--- src/constant/image.js
+++ src/constant/image.js
@@ -1,3 +1,3 @@
 import env from "../config/env";
-export const FILE_PATH = env.apiUrl + "/api/v1/file/";
+export const FILE_PATH = env.staticFileUrl + "/api/v1/file/";
 export const DEFAULT_AVATAR = "avatar_default.png";
